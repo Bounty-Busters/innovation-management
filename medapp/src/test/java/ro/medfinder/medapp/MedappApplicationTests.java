1606004689795d2c6016983a224f4a1e50fb5503
timoteicopaciu <timoteicopaciu99@gmail.com>
2026-06-15 23:54:37 +0300

    add medapp

---
+++ medapp/src/test/java/ro/medfinder/medapp/MedappApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.medfinder.medapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
